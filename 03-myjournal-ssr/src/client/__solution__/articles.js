55470f20880da0cb0471e26d3204f705ec4e2890
Field Peerapong <me@peerapong.dev>
2023-10-20 23:40:05 +0700

    fix(03-ssr): use React on frontend for article list page

--- 03-myjournal-ssr/src/client/__solution__/articles.js
+++ 03-myjournal-ssr/src/client/__solution__/articles.js
@@ -5,7 +5,8 @@ import { ArticleListPage } from "../js/pages/ArticleListPage";
 
 const articles = JSON.parse(document.getElementById("__DATA__").innerHTML);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.hydrateRoot(
+  document.getElementById("root"),
   <React.StrictMode>
     <ArticleListPage data={articles} />
   </React.StrictMode>

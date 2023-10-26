e2bbe524b6496443c0e928cf3746154f6befa107
Field Peerapong <me@peerapong.dev>
2023-10-27 01:14:49 +0700

    next: add get article api

--- 04-myjournal-next/src/pages/api/articles/[id].js
+++ 04-myjournal-next/src/pages/api/articles/[id].js
@@ -8,6 +8,8 @@ import { db } from "@/db";
 export default async function handler(req, res) {
   if (req.method === "PUT") {
     res.json({ data: await db.updateArticle(req.query.id, req.body) });
+  } else if (req.method === "GET") {
+    res.json({ data: await db.getArticle(req.query.id) });
   } else {
     res.status(405);
     res.end();
